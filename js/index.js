583301cc22ecd56897bba9b890d2a7fee725add5
bilalozair <bo569@nyu.edu>
2022-11-13 13:37:31 -0500

    Fix Fetch API for random pokemon search
    I changed the original idea of creating an array of urls by fetching just the pokemon names and urls and then looping over the elements of the array to get each pokemon and its stats. Now I'm directly request at the /pokemon end point and creating an array of pokemon Objects to be displayed.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,53 @@
-document.addEventListener('DOMContentLoaded', dispAllPokemon)
+  const displayPokemon = async () => {
+    // fetch call to API for 20 random pokemon
 
-const dispAllPokemon = () => {
+    
+    const retrievedPokemon = await fetchPokemon();
+    console.log(retrievedPokemon)
+    const pokemonGrid = document.getElementById('pokemon-display-section');
+    retrievedPokemon.map( pokemon => {
+        pokemonGrid.innerHTML += `
+        <img src=${pokemon.image} class = 'pokemon-image'>
+        <h2>${pokemon.name}</h2>  
+        </br> 
+    `
+}
+        
+)}
 
-// Start the API request at a random pokemon
-const max = 1154 - 20;
-const min = 0;
-const firstPokemon = Math.floor(Math.random() * (max - min + 1)) + min;
-const pageLimit = 20;
+const fetchPokemon = async () => {
+/// Writing code to start the API request at a random pokemon
+    const max = 649;
+    const min = 1;
+    const firstPokemon = Math.floor(Math.random() * (max - min + 1)) + min;
+    const pageLimit = 20;  
 
-//Build fetch request
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${firstPokemon}&limit=${pageLimit}`
-fetch(url)
-.then(res => res.json())
-.then(data => console.log(data))
+    /// Fetching Pokemon name, url and img to display to DOM
+    const url = `https://pokeapi.co/api/v2/pokemon/?offset=${firstPokemon}&limit=${pageLimit}`;
+
+    let res = await fetch(url);
+    let pokemonData = await res.json();
+    console.log(pokemonData)
+    const  pokemonList = createPokeObj(firstPokemon,pokemonData);
+    return pokemonList;
 
 }
+
+const createPokeObj = (firstPokemon, pokemonData) => {
+    const pokeObj = pokemonData.results.map((pokemon, index) => (
+        {        
+            id: firstPokemon + index + 1,
+            name: pokemon.name,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${firstPokemon +index + 1}.svg`
+        
+        }
+    ))
+    return pokeObj
+}
+
+
+       
+
+
+document.addEventListener('DOMContentLoaded', displayPokemon)
+
